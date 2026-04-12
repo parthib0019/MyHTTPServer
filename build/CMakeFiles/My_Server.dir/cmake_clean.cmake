595fcc5fe63d0419df25a95f7042a40f7ca7063e
parthib0019 <parthib2290352@gmail.com>
2026-04-12 19:49:38 +0530

    feature:online server first commit

---
+++ build/CMakeFiles/My_Server.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/My_Server.dir/link.d"
+  "CMakeFiles/My_Server.dir/main.c.o"
+  "CMakeFiles/My_Server.dir/main.c.o.d"
+  "CMakeFiles/My_Server.dir/src/myserver.c.o"
+  "CMakeFiles/My_Server.dir/src/myserver.c.o.d"
+  "My_Server"
+  "My_Server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/My_Server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/My_Server.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for My_Server.
+# This may be replaced when dependencies are built.
